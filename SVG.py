3a9158bd02fdf5a1105fff461648c6a55d6aee08
Kladderadatasch <45425556+Kladderadatasch@users.noreply.github.com>
2018-12-19 12:03:35 +0100

    Dynamic Grid + Rectangles

--- SVG.py
+++ SVG.py
@@ -8,21 +8,23 @@ import cgi
 cgitb.enable(format = 'text')
 from jinja2 import Environment, FileSystemLoader
 
-#HTML string
+#Getting coordinates from DB
 def coordinatesHtml():
     conn = cx_Oracle.connect("student/train@geosgen")
     c = conn.cursor()
     c.execute("SELECT HIX,HIY FROM GISTEACH.FIELDS")
-
+    
     list = []
     for row in c:
         list.append(row)
-
-
+    
+    
     conn.close()
     return list
 
 #Render the template
+#Create Framework
+    #Encapsulate CSS in external files
 def print_html():
     env = Environment(loader = FileSystemLoader('.'))
     temp = env.get_template('SVG.html')
@@ -41,7 +43,7 @@ top:5%; left:5%; width:90%; height:90%; background:#fff;\n
 } .face { stroke:#000; stroke-width:20px; stroke-linecap:round }\n</style>\n
 </head>\n<body>''')
 
-#remove .face later
+    #remove .face later
 #    print('''Content-Type: text/html\n\n\
 #<!DOCTYPE html>\n\
 #<head>\n\
@@ -52,57 +54,60 @@ top:5%; left:5%; width:90%; height:90%; background:#fff;\n
 #</style>\n
 #</head>\n<body>''')
     
-    '''SVG Extent'''
+    '''Coordinate Transformation'''
 
     X = []
     Y = []
     
     for row in range(len(allFields)):
     
-        X.append(allFields[row][0]*50)
-        Y.append(allFields[row][1]*50)
-    
-        maximum = []
-        if max(X[::]) >= max(Y[::]):
-            maximum = max(X)
-        else:
-            maximum = max(Y)
+        X.append(allFields[row][0])
+        Y.append(allFields[row][1])
     
     
-            #    CM in Pixel converting ?
-    pixExt = str(maximum)
-    print('''<svg width='''+pixExt+''' height='''+pixExt+'''>\n''')
-    
+    maxcoord = []
+    if max(X[::]) >= max(Y[::]):
+        maxcoord = max(X)
+    else:
+        maxcoord = max(Y)
 
-#    Grid Computing - Necessary to have the Coordinates as Integers
-#    Static values so far, not fitted to screen size
+    '''Dynamic SVG Extent'''    
 
+    print('''<svg width=100% height=100% >\n''')
+    
+    '''Grid Computing'''
+
+    gridmaximum = 100
     i = 1
     while True:
     
-        ticks = str(int((i/len(allFields))*maximum))
-        print('''<line x1='''+ticks+''' y1="0" x2='''+ticks+''' y2='''+str(maximum)+''' style="stroke:rgb(0,0,0);stroke-width:2" /> ''')
-        print('''<line x1="0" y1='''+ticks+''' x2='''+str(maximum)+''' y2='''+ticks+''' style="stroke:rgb(0,0,0);stroke-width:2" /> ''')
+#        ticks = str(int((i/len(allFields))*maximum))
+        ticks = str(float((i/len(allFields))*gridmaximum)) 
+        
+        print('''<line x1='''+ticks+'''% y1=0 x2='''+ticks+'''% y2='''+str(gridmaximum)+'''% style="stroke:rgb(0,0,0);stroke-width:2" /> ''')
+        print('''<line x1=0 y1='''+ticks+'''% x2='''+str(gridmaximum)+'''% y2='''+ticks+'''% style="stroke:rgb(0,0,0);stroke-width:2" /> ''')
         i = i + 1
         if i == len(allFields):
-            ticks = str(int((i/len(allFields))*maximum))
-            print('''<line x1='''+ticks+''' y1="0" x2='''+ticks+''' y2='''+str(maximum)+''' style="stroke:rgb(0,0,0);stroke-width:4" /> ''')
-            print('''<line x1="0" y1='''+ticks+''' x2='''+str(maximum)+''' y2='''+ticks+''' style="stroke:rgb(0,0,0);stroke-width:4" /> ''')
+            ticks = str(int((i/len(allFields))*gridmaximum))
+            print('''<line x1='''+ticks+'''% y1=0 x2='''+ticks+'''% y2='''+str(gridmaximum)+'''% style="stroke:rgb(0,0,0);stroke-width:4" /> ''')
+            print('''<line x1=0 y1='''+ticks+'''% x2='''+str(gridmaximum)+'''% y2='''+ticks+'''% style="stroke:rgb(0,0,0);stroke-width:4" /> ''')
             break
 
 
 
-#   Rectangle Computing
+    '''Rectangle Computing'''
+    
 #   Fitted to Screen Size + Centralized
 #   Fill + transparency + hovering
 #   Labelling + hovering [Owner + Crop + Extent]
 #   OnClick [Popup of finds at coordinates]
+    
     for row in range(len(X)):
-        rectX = str(X[row])
-        rectY = str(Y[row])
+        rectX = str((X[row]/maxcoord)*100)
+        rectY = str((Y[row]/maxcoord)*100)
 
         #HTML Body
-        print ('''<rect width='''+rectX+''' height='''+rectY+''' \
+        print ('''<rect width='''+rectX+'''% height='''+rectY+'''% \
 style="fill:none;stroke-width:3;stroke:rgb(255,0,0)" />''')
 
     #HTML Framkework
