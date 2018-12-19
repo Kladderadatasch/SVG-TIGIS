d4bf0f1d2edca3a823fb4c87eb696eb017b74ce5
Kladderadatasch <45425556+Kladderadatasch@users.noreply.github.com>
2018-12-19 22:02:56 +0100

    Color + Opacity

--- SVG.py
+++ SVG.py
@@ -40,8 +40,40 @@ body { background:#eee; margin:0 }\n
 svg {\n
 display:block; border:1px solid #ccc; position:absolute;\n
 top:5%; left:5%; width:90%; height:90%; background:#fff;\n
-} .face { stroke:#000; stroke-width:20px; stroke-linecap:round }\n</style>\n
-</head>\n<body>''')
+} .face { stroke:#000; stroke-width:20px; stroke-linecap:round }\n''')
+    
+    '''Dynamic InLine CSS'''
+#   Add CSS above
+    colorramp = ["F0F8FF","FAEBD7","00FFFF","7FFFD4","F0FFFF","F5F5DC","FFE4C4",
+                 0,"FFEBCD","0000FF","8A2BE2","A52A2A","DEB887","5F9EA0",
+                 "7FFF00","D2691E","FF7F50","6495ED","FFF8DC","DC143C",
+                 "00FFFF","00008B","008B8B","B8860B","A9A9A9","A9A9A9",
+                 6400,"BDB76B","8B008B","556B2F","FF8C00","9932CC","8B0000",
+                 "E9967A","8FBC8F","483D8B","2F4F4F","2F4F4F","00CED1",
+                 "9400D3","FF1493","00BFFF",696969,696969,"1E90FF",
+                 "B22222","FFFAF0","228B22","FF00FF","DCDCDC","F8F8FF",
+                 "FFD700","DAA520",808080,808080,8000,"ADFF2F","F0FFF0",
+                 "FF69B4","CD5C5C","4B0082","FFFFF0","F0E68C","E6E6FA",
+                 "FFF0F5","7CFC00","FFFACD","ADD8E6","F08080","E0FFFF",
+                 "FAFAD2","D3D3D3","D3D3D3","90EE90","FFB6C1","FFA07A",
+                 "20B2AA","87CEFA",778899,778899,"B0C4DE","FFFFE0","00FF00",
+                 "32CD32","FAF0E6","FF00FF",800000,"66CDAA","0000CD","BA55D3",
+                 "9370DB","3CB371","7B68EE","00FA9A","48D1CC","C71585",191970,
+                 "F5FFFA","FFE4E1","FFE4B5","FFDEAD",80,"FDF5E6",808000,
+                 "6B8E23","FFA500","FF4500","DA70D6","EEE8AA","98FB98",
+                 "AFEEEE","DB7093","FFEFD5","FFDAB9","CD853F","FFC0CB",
+                 "DDA0DD","B0E0E6",800080,663399,"FF0000","BC8F8F",41690,
+                 "8B4513","FA8072","F4A460","2E8B57","FFF5EE","A0522D",
+                 "C0C0C0","87CEEB","6A5ACD",708090,708090,"FFFAFA","00FF7F",
+                 "4682B4","D2B48C",8080,"D8BFD8","FF6347","40E0D0","EE82EE",
+                 "F5DEB3","FFFFFF","F5F5F5","FFFF00","9ACD32"]
+
+    for row in range(len(allFields)):
+        print('''#r'''+str(row)+''' {
+fill: #'''+str(colorramp[row])+''';\nstroke-width: 3;\nfill-opacity: 0.5;}\n''')
+           
+
+    print('''</style>\n</head>\n<body>''')
 
     #remove .face later
 #    print('''Content-Type: text/html\n\n\
@@ -53,7 +85,7 @@ top:5%; left:5%; width:90%; height:90%; background:#fff;\n
 #svg { position:fixed; top:0; bottom:0; left:0; right:0 }\n
 #</style>\n
 #</head>\n<body>''')
-    
+
     '''Coordinate Transformation'''
 
     X = []
@@ -101,15 +133,16 @@ top:5%; left:5%; width:90%; height:90%; background:#fff;\n
 #   Fill + transparency + hovering
 #   Labelling + hovering [Owner + Crop + Extent]
 #   OnClick [Popup of finds at coordinates]
-    
+   
     for row in range(len(X)):
         rectX = str((X[row]/maxcoord)*100)
         rectY = str((Y[row]/maxcoord)*100)
-
+        
         #HTML Body
-        print ('''<rect width='''+rectX+'''% height='''+rectY+'''% \
-style="fill:none;stroke-width:3;stroke:rgb(255,0,0)" />''')
-
+        print ('''<rect width='''+rectX+'''% height='''+rectY+'''% class="rectangle" \
+id="r'''+str(row)+'''" />''')
+       
+        
     #HTML Framkework
     print("</svg>\n</body>\n</html>")
 
